88fe2d728db0221cbdf47dc6b4d69038bd2fc4d8
Matheus Carvalho <matheuscarvalho210404@outlook.com>
2024-04-11 15:27:34 -0300

    arrumando o formula kite men bronze

--- scraping_2024/fixing_data.py
+++ scraping_2024/fixing_data.py
@@ -12,7 +12,6 @@ corrida2.drop(corrida2.columns[0], axis=1, inplace=True)
 
 corrida1.to_csv('kite_wc_women_over_gold.csv', index=False)
 corrida2.to_csv('kite_wc_women_over_silver.csv', index=False)
-"""
 
 df = pd.read_csv("scraping_2024/kite_wc_men_over.csv")
 indices_transicao = df[df['RANK'] == "1ST"].index
@@ -28,3 +27,16 @@ corrida3.drop(corrida2.columns[0], axis=1, inplace=True)
 corrida1.to_csv('kite_wc_men_over_gold.csv', index=False)
 corrida2.to_csv('kite_wc_men_over_silver.csv', index=False)
 corrida3.to_csv('kite_wc_men_over_bronze.csv', index=False)
+
+"""
+
+df_1 = pd.read_csv("scraping_2024\kite_wc_men_over_bronze.csv")
+df_2 = pd.read_csv("scraping_2024\kite_wc_men_over_silver.csv")
+
+list_aux = list(df_2[df_2.columns[0]])
+list_aux.append("31ST")
+
+df_1.drop(df_1.columns[0], axis=1, inplace=True)
+
+df_1["RANK"] = list_aux
+df_1.to_csv('kite_wc_men_over_bronze.csv', index=False)
