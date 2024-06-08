5ee5eac572c83c60308ac5d001a537ef48cddf13
Gab-Mel <gabrielzinho.melo.10@gmail.com>
2024-06-08 16:25:39 -0300

    nothing

--- web/next.config.js
+++ web/next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   reactStrictMode: true,
   swcMinify: true,
-  basePath: "/sailing-rankings-2024.1",
 }
 
 module.exports = nextConfig
